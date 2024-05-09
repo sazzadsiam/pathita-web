43fbd9be008a9f1acb6e5ea07d384361cc87b6a2
Sazzad Siam <sazzadsiam113@gmail.com>
2024-05-09 11:40:18 +0600

    simple change

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,8 +11,8 @@ const Navbar = () => {
     const [menu, setMenu] = useState(false);
 
     return (
-        <nav className="relative flex justify-between items-center px-6 sm:px-20 h-[74px] border-b border-black bg-white">
-            <div className="flex justify-between w-full sm:w-auto">
+        <nav className="relative flex justify-between items-center sm:px-20 h-[74px] border-b border-black bg-white">
+            <div className="flex justify-between items-center w-full sm:w-auto z-10 h-full px-6 sm:px-0 bg-white">
                 <Link href={'./'}><Image src={logo} alt="logo" /></Link>
                 <span className="sm:hidden">
                     {
@@ -26,7 +26,7 @@ const Navbar = () => {
 
 
 
-            <ul className={`absolute sm:static left-0 ${menu ? 'top-[74px]' : 'top-[-300px]'} z-[-1] sm:z-0 w-full sm:w-auto flex flex-col sm:flex-row sm:items-center py-5 sm:py-0 px-6 sm:px-0 gap-x-10 gap-y-6 sm:gap-y-0 font-primary text-sm bg-white drop-shadow-lg sm:drop-shadow-none transition-all ease-in duration-[400ms]`}>
+            <ul className={`absolute sm:static left-0 ${menu ? 'top-[74px]' : 'top-[-300px]'} w-full sm:w-auto flex flex-col sm:flex-row sm:items-center py-5 sm:py-0 px-6 sm:px-0 gap-x-10 gap-y-6 sm:gap-y-0 font-primary text-sm bg-white drop-shadow-lg sm:drop-shadow-none transition-all ease-in duration-[400ms]`}>
                 <Link href={'#'} className="navLink">ব্লগ</Link>
                 <Link href={'#'} className="navLink">বই</Link>
                 <Link href={'#'} className="navLink">লিখুন</Link>
