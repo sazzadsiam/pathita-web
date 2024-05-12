95c8d7e1105945dfa53e6c0f65c65632554832c5
Sazzad Siam <sazzadsiam113@gmail.com>
2024-05-12 14:08:19 +0600

    simple fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,10 +11,10 @@ const Navbar = () => {
     const [menu, setMenu] = useState(false);
 
     return (
-        <nav className="relative flex justify-between items-center sm:px-20 h-[74px] border-b border-black bg-white">
-            <div className="flex justify-between items-center w-full sm:w-auto z-10 h-full px-6 sm:px-0 bg-white">
+        <nav className="relative flex justify-between items-center md:px-20 h-[74px] border-b border-black bg-white">
+            <div className="flex justify-between items-center w-full md:w-auto z-10 h-full px-6 md:px-0 bg-white">
                 <Link href={'./'}><Image src={logo} alt="logo" /></Link>
-                <span className="sm:hidden">
+                <span className="md:hidden">
                     {
                         menu ?
                             <FaXmark className="text-3xl" onClick={() => setMenu(false)} />
@@ -26,12 +26,12 @@ const Navbar = () => {
 
 
 
-            <ul className={`absolute sm:static left-0 ${menu ? 'top-[74px]' : 'top-[-300px]'} w-full sm:w-auto flex flex-col sm:flex-row sm:items-center py-5 sm:py-0 px-6 sm:px-0 gap-x-10 gap-y-6 sm:gap-y-0 font-primary text-sm bg-white drop-shadow-lg sm:drop-shadow-none transition-all ease-in duration-[400ms]`}>
+            <ul className={`absolute md:static left-0 ${menu ? 'top-[74px]' : 'top-[-300px]'} w-full md:w-auto flex flex-col md:flex-row md:items-center py-5 md:py-0 px-6 md:px-0 gap-x-10 gap-y-6 md:gap-y-0 font-primary text-sm bg-white drop-shadow-lg md:drop-shadow-none transition-all ease-in duration-300`}>
                 <Link href={'#'} className="navLink">ব্লগ</Link>
                 <Link href={'#'} className="navLink">বই</Link>
                 <Link href={'#'} className="navLink">লিখুন</Link>
                 <Link href={'#'} className="navLink">আমাদের গল্প</Link>
-                <button className="bg-gray text-white font-medium px-4 py-2 rounded-3xl">শুরু করুন</button>
+                <button className="bg-gray text-white font-medium px-4 py-2 rounded-3xl hover:bg-primary hover:text-gray transition-all duration-300">শুরু করুন</button>
             </ul>
         </nav>
     )
